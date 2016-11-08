cd4b9850eeeafa9615af102af7e1054521a2fd10
migbro <miguel.a.brown@gmail.com>
2016-11-08 14:23:22 -0600

    Added ability to iterate over other primer lengths if no result found with starting input length

--- crispr_donor_pipe.py
+++ crispr_donor_pipe.py
@@ -27,7 +27,8 @@ args = docopt(__doc__)
 def parse_config(config_file):
     config_data = json.loads(open(config_file, 'r').read())
     return config_data['primer3'], config_data['master'], config_data['Lsettings'], config_data['Rsettings'], \
-           config_data['lf_gibson'], config_data['lr_gibson'], config_data['rf_gibson'], config_data['rr_gibson']
+           config_data['lf_gibson'], config_data['lr_gibson'], config_data['rf_gibson'], config_data['rr_gibson'], \
+           config_data['GC_trigger']
 
 
 def populate_seq_dict(id_dict, master, err):
@@ -60,21 +61,21 @@ def rev_comp(seq):
     return new_seq[::-1]
 
 
-def create_seq(nm, info, LR_len, RF_len):
-        l_input_file = temp_dir + nm + '_LEFT_SEQUENCE.txt'
-        r_input_file = temp_dir + nm + '_RIGHT_SEQUENCE.txt'
-        left = open(l_input_file, 'w')
-        lr_prime = info['seq'][0][(int(LR_len) * -1):]
-        lr_prime = rev_comp(lr_prime)
-        left.write('SEQUENCE_ID=' + nm + '\nSEQUENCE_TEMPLATE=' + info['seq'][0] + '\nSEQUENCE_PRIMER_REVCOMP='
-                   + lr_prime + '\nSEQUENCE_TARGET=37,21\n=')
-        left.close()
-        rf_prime = info['seq'][1][:(int(RF_len))]
-        right = open(r_input_file, 'w')
-        right.write('SEQUENCE_ID=' + nm + '\nSEQUENCE_TEMPLATE=' + info['seq'][1] + '\nSEQUENCE_PRIMER='
-                   + rf_prime + '\nSEQUENCE_TARGET=37,21\n=')
-        right.close()
-        return l_input_file, r_input_file
+def create_seq(nm, info, p_len, side):
+        input_file = temp_dir + nm + '_' + side + '_SEQUENCE.txt'
+        out_file = open(input_file, 'w')
+        if side == 'LEFT':
+            prime = info['seq'][0][(int(p_len) * -1):]
+            prime = rev_comp(prime)
+            out_file.write('SEQUENCE_ID=' + nm + '\nSEQUENCE_TEMPLATE=' + info['seq'][0] + '\nSEQUENCE_PRIMER_REVCOMP='
+                   + prime + '\nSEQUENCE_TARGET=37,21\n=')
+            out_file.close()
+        else:
+            prime = info['seq'][1][:(int(p_len))]
+            out_file.write('SEQUENCE_ID=' + nm + '\nSEQUENCE_TEMPLATE=' + info['seq'][1] + '\nSEQUENCE_PRIMER='
+                   + prime + '\nSEQUENCE_TARGET=37,21\n=')
+            out_file.close()
+        return input_file
 
 
 def parse_results(output, forward, reverse, side, gene):
@@ -83,25 +84,28 @@ def parse_results(output, forward, reverse, side, gene):
     attr_dict = {'PRIMER_LEFT_0_PROBLEMS': '', 'PRIMER_LEFT_0_TM': '', 'PRIMER_RIGHT_0_PROBLEMS': '',
                  'PRIMER_RIGHT_0_TM': ''}
     f = 0
+    fixed = ''
     for result in open(output):
         cur = result.rstrip('\n').split('=')
         if cur[0] in attr_dict:
             attr_dict[cur[0]] = cur[1]
         if side == 'Left' and cur[0] == 'SEQUENCE_PRIMER_REVCOMP':
             r_primer = cur[1]
+            fixed = r_primer
         elif side == 'Left' and cur[0] == 'PRIMER_LEFT_0_SEQUENCE':
             f_primer = cur[1]
             f = 1
             #break
         if side == 'Right' and cur[0] == 'SEQUENCE_PRIMER':
             f_primer = cur[1]
+            fixed = f_primer
         elif side == 'Right' and cur[0] == 'PRIMER_RIGHT_0_SEQUENCE':
             r_primer = cur[1]
             f = 1
             #break
     return '\t'.join((gene + '.' + side + '.F', forward + f_primer, attr_dict['PRIMER_LEFT_0_PROBLEMS'],
                       attr_dict['PRIMER_LEFT_0_TM'], gene + '.' + side + '.R', reverse + r_primer,
-                      attr_dict['PRIMER_RIGHT_0_PROBLEMS'], attr_dict['PRIMER_RIGHT_0_TM'])), f,
+                      attr_dict['PRIMER_RIGHT_0_PROBLEMS'], attr_dict['PRIMER_RIGHT_0_TM'])), f, fixed
 
 
 def run_primer3(input, output, settings, primer3):
@@ -109,22 +113,63 @@ def run_primer3(input, output, settings, primer3):
     subprocess.call(cmd, shell=True)
 
 
+def calc_gc(seq):
+    slen = len(seq)
+    gc_ct = 0.0
+    gc_dict = {'G': 0.0, 'C': 0.0}
+    for nt in seq:
+        if nt in gc_dict:
+            gc_ct += 1.0
+            gc_dict[nt] += 1.0
+    return gc_ct/slen
+
+
 def setup_primer3(seq_dict, primer3, Lsettings, Rsettings, temp_dir, LR_len, RF_len, lf_gibson, lr_gibson, rf_gibson,
-                  rr_gibson, tbl, min_len, max_len):
+                  rr_gibson, tbl, min_len, max_len, gc_trigger):
     for nm in seq_dict:
-        (l_input_file, r_input_file) = create_seq(nm, seq_dict[nm], LR_len, RF_len)
+        l_input_file = create_seq(nm, seq_dict[nm], LR_len, 'LEFT')
+        r_input_file = create_seq(nm, seq_dict[nm], RF_len, 'RIGHT')
         l_output_file = temp_dir + nm + '_LEFT_PRIMER3_RESULTS.txt'
         r_output_file = temp_dir + nm + '_RIGHT_PRIMER3_RESULTS.txt'
         gene = seq_dict[nm]['gene']
         run_primer3(l_input_file, l_output_file, Lsettings, primer3)
         run_primer3(r_input_file, r_output_file, Rsettings, primer3)
         # parse results, if primer not found, adjust length and try again
-        (left_str, left_flag) = parse_results(l_output_file, lf_gibson, lr_gibson, 'Left',  gene)
-        (right_str, right_flag) = parse_results(r_output_file, rf_gibson, rr_gibson, 'Right', gene)
+        (left_str, left_flag, left_fixed) = parse_results(l_output_file, lf_gibson, lr_gibson, 'Left', gene)
+        if left_flag == 0:
+            cur_gc = calc_gc(left_fixed)
+            start = int(LR_len) + 1
+            stop = int(max_len) + 1
+            if cur_gc >= gc_trigger:
+                start = int(min_len)
+                stop = int(LR_len)
+            for i in xrange(start, stop, 1):
+                l_input_file = create_seq(nm, seq_dict[nm], str(i), 'LEFT')
+                l_output_file = temp_dir + nm + '_LEFT_PRIMER3_RESULTS.txt'
+                run_primer3(l_input_file, l_output_file, Lsettings, primer3)
+                (left_str, left_flag, left_fixed) = parse_results(l_output_file, lf_gibson, lr_gibson, 'Left', gene)
+                if left_flag == 1:
+                    break
+        (right_str, right_flag, right_fixed) = parse_results(r_output_file, rf_gibson, rr_gibson, 'Right', gene)
+        if right_flag == 0:
+            cur_gc = calc_gc(right_fixed)
+            start = int(RF_len) + 1
+            stop = int(max_len) + 1
+            if cur_gc >= gc_trigger:
+                start = int(min_len)
+                stop = int(LR_len)
+            for i in xrange(start, stop, 1):
+                r_input_file = create_seq(nm, seq_dict[nm], str(i), 'RIGHT')
+                r_output_file = temp_dir + nm + '_RIGHT_PRIMER3_RESULTS.txt'
+                run_primer3(r_input_file, r_output_file, Rsettings, primer3)
+                (right_str, right_flag, right_fixed) = parse_results(r_output_file, rf_gibson, rr_gibson, 'Right', gene)
+                if right_flag == 1:
+                    break
         tbl.write(nm + '\t' + left_str + '\t' + right_str + '\n')
 
 
-(primer3, master, Lsettings, Rsettings, lf_gibson, lr_gibson, rf_gibson, rr_gibson) = parse_config(args['<config>'])
+(primer3, master, Lsettings, Rsettings, lf_gibson, lr_gibson, rf_gibson, rr_gibson, gc_trigger) = \
+    parse_config(args['<config>'])
 (LR_len, RF_len, min_len, max_len) = (args['<LR_len>'], args['<RF_len>'], args['<min>'], args['<max>'])
 timestamp = time.strftime("%Y-%m-%d_%H%M") + '_' + LR_len + '_' + RF_len
 warnings = open(timestamp + '_warnings.txt', 'w')
@@ -145,5 +190,5 @@ for line in open(args['<list>']):
 # get relevant seqs from table
 seq_dict = populate_seq_dict(id_dict, master, warnings)
 setup_primer3(seq_dict, primer3, Lsettings, Rsettings, temp_dir, LR_len, RF_len, lf_gibson, lr_gibson, rf_gibson,
-              rr_gibson, tbl, min_len, max_len)
+              rr_gibson, tbl, min_len, max_len, gc_trigger)
 tbl.close()
