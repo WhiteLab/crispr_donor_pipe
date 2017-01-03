3bdf551983d02e77fb432a4fbe29a8e883336b7f
migbro <miguel.a.brown@gmail.com>
2017-01-03 13:45:27 -0600

    Created parsing script for guide output from crispr.mit.edu

---
+++ crispr_guide_select.py
@@ -0,0 +1,83 @@
+"""Parses pasted output from http://crispr.mit.edu/ and select best guide
+
+Usage: ./crispr_guide_select.py (<flank> <pasted>) [options]
+
+Arguments:
+    <flank>    100 bp flanked stop codon reference file
+    <pasted>      pasted output from browser
+
+Options:
+    -h --help
+
+"""
+import re
+from docopt import docopt
+args = docopt(__doc__)
+from crispr_donor_pipe import rev_comp
+
+
+
+def get_loc(cand, seq):
+    m = re.search(cand, seq)
+    r = 0
+    if m is None:
+        rev_cand = rev_comp(cand)
+        m = re.search(rev_cand, seq)
+        r = 1
+    # using 0 to represent spanning stop codon, 1 for 3' in reference to stop, or 2 in 5' reference to stop
+    (start, end) = m.span()
+    d = start - 102
+    if start <= 100 and end >= 102:
+        return 0, 0, r
+    elif start > 102:
+        return 1, d, r
+    else:
+        d = 100 - end
+        return 2, d, r
+
+
+seq_list = []
+seq_alias = {}
+
+flanks = open(args['<flank>'])
+head = next(flanks)
+ind = 0
+for flank in flanks:
+    info = flank.rstrip('\n').split('\t')
+    ids = info[0].split(',')
+    syms = info[1].split(',')
+    seq_list.append(info[2])
+    for nm in ids:
+        seq_alias[nm] = ind
+    for sym in syms:
+        if sym != 'None':
+            seq_alias[sym] = ind
+    ind += 1
+flanks.close()
+cur = ''
+gRNA = {}
+temp = {}
+for line in open(args['<pasted>']):
+    info = line.rstrip('\n').split('\t')
+    if info[0][0:5] != 'Guide':
+        cur = info[0]
+        gRNA[cur] = {}
+        temp = {}
+    else:
+        (cat, dist, strand) = get_loc(info[2], seq_list[seq_alias[cur]])
+        if cat not in gRNA[cur]:
+            gRNA[cur] = {}
+        gRNA[cur][cat][dist]['name'] = info[0]
+        gRNA[cur][cat][dist]['st'] = strand
+        gRNA[cur][cat][dist]['seq'] = info[2]
+        gRNA[cur][cat][dist]['score'] = info[1]
+# print out prioritized gRNAs
+print 'Gene\tCategory\tDistance\tSequence\tStrand\tName\n'
+cat_dict = {0: 'STOP', 1: 'UTR', 2: 'CODON'}
+st_dict = {0: '+', 1: '-'}
+for gene in gRNA:
+    for i in xrange(3):
+        if i in gRNA[gene]:
+            for j in sorted(gRNA[gene][i]):
+                cur = gRNA[gene][i][j]
+                print '\t'.join((gene, cat_dict[i], cur['dist'], cur['seq'], st_dict[cur['st']], cur['name']))
