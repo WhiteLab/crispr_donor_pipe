55bfb3ccae0b39295dc17a40a94e13556772c538
migbro <miguel.a.brown@gmail.com>
2018-03-20 16:38:05 -0500

    troubleshoot cleanup

--- run_mfold.py
+++ run_mfold.py
@@ -47,9 +47,9 @@ def run_mfold(config, out_dir, seg_name, seq, tm):
                     + seg_name + '.out'
     sys.stderr.write('mfold command ' + run_mfold_cmd + '\n')
     check = subprocess.call(run_mfold_cmd, shell=True)
-    cleanup = 'sh -c \'rm -v !(*.out|*.ps)\''
+    cleanup = 'rm -v !(*.out|*.ps)'
     sys.stderr.write('Cleaning out unneeded temp files ' + cleanup + '\n')
-    subprocess.call(cleanup, shell=True)
+    subprocess.call(['bash', '-c', cleanup])
     os.chdir(cwd)
     sys.stderr.write('Changed back to pipe run dir ' + cwd + '\n')
     if check != 0:
